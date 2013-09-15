24bef8d6503eac889e89e54ca2725f5dcc42cdca
Jonathan Manalus <jmanalus@Jonathans-MacBook-Air.local>
2013-09-14 17:00:04 -0700

    1.1 and 1.2 done

--- hw1/part1.rb
+++ hw1/part1.rb
@@ -1,40 +1,22 @@
 def sum(ints)
-  ints.inject { |sum, i| sum+i}
+  ints.reduce(:+)
 end
 
 def max_2_sum(ints)
-  # WRITE CODE HERE
+  raise Error if ints.length < 2
+  presort = ints.sort
+  presort[-1] + presort[-2]
 end
 
-def max_2_sum(arr)
-  raise ErrorShortArray if arr.length < 2
-=begin
-  maxs = Array.new(2)
-  if (arr[0] < arr[1]) then
-    maxs[0] = arr[0]; maxs[1] = arr[1]
-  else
-    maxs[0] = arr[1]; maxs[1] = arr[0]
-  end 
-  arr.drop(2).each { |i| maxs = update_maxs(maxs, i)}
-  return maxs[0] + maxs[1]
-=end
-  # Shorter, better!
-  sorted = arr.sort
-  sorted[-1] + sorted[-2]
-end
 
-def sum_to_n(ints)
-  # WRITE CODE HERE
-end
-
-def sum_to_n? arr, n
- hash = Hash.new
- arr.each do |val|
-   if hash.key? val
-     return true
-   else
-     hash[n-val] = val
-   end
- end
- return false
+def sum_to_n(ints, n)
+  new_hash = Hash.new
+  ints.each do |val|
+    if new_hash? val
+      return true
+    else
+      new_hash[n-val] = val
+    end
+  end
+  return false
 end
