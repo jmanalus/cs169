772bc470af14f6eeae70d0dd26bc099dc6d4a5ad
Jonathan Manalus <jmanalus@Jonathans-MacBook-Air.local>
2013-09-14 23:07:46 -0700

    commit

--- hw1/part2.rb
+++ hw1/part2.rb
@@ -3,9 +3,18 @@ def hello(name)
 end
 
 def starts_with_consonant?(s)
-	s =~ /^[^aeiou]/i
+	if s =~ /^[^aeiou]/i
+		return true
+	else
+		return false
+	end
 end
-
+ 
 def binary_multiple_of_4?(s)
-  str =~ /^[01]*1[01]*00$/
+	if s =~ /^[01]*1[01]*00$/
+		return true
+	else
+		return false
+	end
 end
+starts_with_consonant?('#foo')
