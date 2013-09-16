882bd6f142ab2fd847f49f1ddad99b5b6b87eddd
Jonathan Manalus <jmanalus@Jonathans-MacBook-Air.local>
2013-09-16 14:35:15 -0700

    hw1 done

--- hw1/part3.rb
+++ hw1/part3.rb
@@ -8,14 +8,14 @@ class BookInStock
   raise ArgumentError if price == 0
   raise ArgumentError if price < 0
   end
-   def price_as_string
-    "ISBN: #{@isbn}, price: #{@price}"
-  end
-    def price_in_cents
-    Integer(@price * 100 + 0.5)
+
+  def price_format
+    (@price * 100 + 0.5)/100.00
   end
 
-  def price_in_cents=(cents)
-    @price = cents / 100.0
+  def price_as_string
+    "$#{@price}"
   end
 end
+
+((1.1 * 100 + 0.5)/100.00).round(3).to_s.chomp(3)
